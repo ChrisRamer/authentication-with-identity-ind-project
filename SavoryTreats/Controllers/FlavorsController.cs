e9a7807bb1f52bd7b72d2b19279513e945b11eb4
Chris Ramer <chrisramer.97@gmail.com>
2021-06-01 10:40:25 -0700

    Add index route & view for flavors

---
+++ SavoryTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using SavoryTreats.Models;
+
+namespace SavoryTreats.Controllers
+{
+	public class FlavorsController : Controller
+	{
+		private readonly SavoryTreatsContext _db;
+
+		public FlavorsController(SavoryTreatsContext db)
+		{
+			_db = db;
+		}
+
+		private Flavor GetFlavorFromId(int id)
+		{
+			return _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
+		}
+
+		public ActionResult Index()
+		{
+			List<Flavor> model = _db.Flavors.ToList();
+			return View(model);
+		}
+	}
+}
