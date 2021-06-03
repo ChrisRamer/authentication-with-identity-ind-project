25b08cf580f0f6e4b9250c635a657820556bd5a5
Chris Ramer <chrisramer.97@gmail.com>
2021-06-03 10:39:12 -0700

    Add login route & view for account

--- SavoryTreats/Controllers/AccountController.cs
+++ SavoryTreats/Controllers/AccountController.cs
@@ -44,5 +44,25 @@ namespace SavoryTreats.Controllers
 				return View();
 			}
 		}
+
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Login(LoginViewModel model)
+		{
+			Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
 	}
 }
