b4b6db66012d6a1e008e8811d4e6652ee3c58d61
Chris Ramer <chrisramer.97@gmail.com>
2021-06-04 11:13:04 -0700

    Add list of flavors & treats to homepage

--- SavoryTreats/Controllers/HomeController.cs
+++ SavoryTreats/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SavoryTreats.Models;
 
 namespace SavoryTreats.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly SavoryTreatsContext _db;
+
+		public HomeController(SavoryTreatsContext db)
+		{
+			_db = db;
+		}
+
 		[HttpGet("/")]
 		public ActionResult Index()
 		{
+			ViewBag.Flavors = _db.Flavors.ToList();
+			ViewBag.Treats = _db.Treats.ToList();
 			return View();
 		}
 	}
